43e30859e68ea33285c0f1b52b9f478172d57a1d
Рыбина Лидия <lidiar@rambler.ru>
2025-10-24 22:25:01 +0500

    add hw_13.py and test

--- src/hw_13.py
+++ src/hw_13.py
@@ -1,18 +1,7 @@
 from importlib.metadata import pass_none
-from os import path
-
-import pandas as pd
-import openpyxl
-import csv
 import json
-
-
-
-def get_file_scv(filename: str) -> pd.DataFrame:
-    df = pd.read_csv(filename)
-    return df
-
 import pandas as pd
+from pandas.core.interchange.dataframe_protocol import DataFrame
 
 
 def avg_age_by_gender(df):
@@ -34,7 +23,16 @@ def fare_per_passenger_by_class(df):
     return json.dumps(result_dict)
 
 
+def get_file_scv(filename: str) -> pd.DataFrame:
+    df = pd.read_csv(filename)
+    return df
+
+def get_filter_sort_df(df: DataFrame) -> pd.DataFrame:
+    new_df = df[(df['Age'] < 30) & (df['Age'] < 30)].sort_values('PassengerId')
+    return new_df
+
 df_titanic = get_file_scv('..\\data\\titanic.csv')
 print(get_file_scv('..\\data\\titanic.csv').head())
 print(avg_age_by_gender(df_titanic))
+print(get_filter_sort_df(df_titanic))
 
